4c4a750f26302482b63bed3a8a6a2d7f22079336
derron <derron210@gmail.com>
2020-07-16 20:08:11 +0300

    Добавьте файлы проекта.

---
+++ Roll/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Roll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxBound = 100;
+            if (args.Length > 0)
+            {
+                if (!Int32.TryParse(args[0], out maxBound))
+                {
+                    Console.WriteLine("Usage: roll [number]. Example: roll 100");
+                    return;
+                }
+            }
+            Random rand = new Random();
+            Console.WriteLine(rand.Next(maxBound) + 1);
+        }
+    }
+}
